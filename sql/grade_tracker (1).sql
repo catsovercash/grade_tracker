345fc67d1316412ef8d744ecaebd0a4041159c43
John Carlo Nayan <50688138+catsovercash@users.noreply.github.com>
2026-09-25 17:46:20 +0800

    +New SQL File

--- sql/grade_tracker (1).sql
+++ sql/grade_tracker (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 18, 2026 at 02:11 PM
+-- Generation Time: Sep 25, 2026 at 11:45 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -20,6 +20,52 @@ SET time_zone = "+00:00";
 --
 -- Database: `grade_tracker`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `components`
+--
+
+CREATE TABLE `components` (
+  `component_id` int(11) NOT NULL,
+  `component_name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `components`
+--
+
+INSERT INTO `components` (`component_id`, `component_name`) VALUES
+(1, 'Quizzes & Seatwork'),
+(2, 'Midterm Examination'),
+(3, 'Final Project & Exam'),
+(4, 'Assignments & Homework'),
+(5, 'Attendance & Participation'),
+(6, 'Laboratory Activities');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `grade_templates`
+--
+
+CREATE TABLE `grade_templates` (
+  `template_id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `course_code` varchar(20) DEFAULT NULL,
+  `course_title` varchar(100) DEFAULT NULL,
+  `class_code` varchar(30) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `grade_templates`
+--
+
+INSERT INTO `grade_templates` (`template_id`, `user_id`, `course_code`, `course_title`, `class_code`, `created_at`) VALUES
+(1, 1, 'CS-301', 'Software Engineering', 'CS301-SEC6', '2026-09-25 09:17:48'),
+(2, 1, 'CS-301', 'Software Engineering', 'CS301-SEC9', '2026-09-25 09:19:49');
 
 -- --------------------------------------------------------
 
@@ -92,6 +138,31 @@ INSERT INTO `role_permissions` (`role_id`, `permission_id`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `template_components`
+--
+
+CREATE TABLE `template_components` (
+  `id` int(11) NOT NULL,
+  `template_id` int(11) DEFAULT NULL,
+  `component_id` int(11) DEFAULT NULL,
+  `weight` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `template_components`
+--
+
+INSERT INTO `template_components` (`id`, `template_id`, `component_id`, `weight`) VALUES
+(1, 1, 1, 30),
+(2, 1, 2, 30),
+(3, 1, 3, 40),
+(4, 2, 4, 50),
+(5, 2, 1, 20),
+(6, 2, 3, 30);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -109,7 +180,11 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`user_id`, `full_name`, `username`, `email`, `password_hash`, `created_at`) VALUES
-(1, 'John Carlo Nayan', 'catsovercash', NULL, '$2y$10$XHPI3aezc537I/kqKhKvPeuR1CHH9B/PSzZutSHbs1J/YwgBf1.uK', '2026-09-18 11:42:02');
+(1, 'John Carlo Nayan', 'catsovercash', NULL, '$2y$10$XHPI3aezc537I/kqKhKvPeuR1CHH9B/PSzZutSHbs1J/YwgBf1.uK', '2026-09-18 11:42:02'),
+(2, 'ear', 'ear', NULL, '$2y$10$2p7d1hsebkPcOKEefG.sEOvChF7zVE2jz0eMZErL.bjmPHhEoudyi', '2026-09-25 05:26:25'),
+(3, '123123123', '123123123', NULL, '$2y$10$75ODIti6brphStkmuWQ49u.i3XVmV8H88kV/LejHnwaSx7FupisEW', '2026-09-25 05:31:14'),
+(4, 'earlbaterbonia', 'earlbaterbonia', NULL, '$2y$10$3z.5jAqXlAl36.I80.9PX.1cCoO4ZVQYwej0k7T1voYLIH3s2uP4a', '2026-09-25 07:20:13'),
+(5, 'Kiyumeeehh', 'kiyu', NULL, '$2y$10$yqRrOGGQMj/LnGyeYVw9uOfEHVebxBZNZ8/qvepbZOON3/gu.aS.a', '2026-09-25 07:21:52');
 
 -- --------------------------------------------------------
 
@@ -127,11 +202,29 @@ CREATE TABLE `user_roles` (
 --
 
 INSERT INTO `user_roles` (`user_id`, `role_id`) VALUES
-(1, 3);
+(1, 3),
+(2, 3),
+(3, 3),
+(4, 3),
+(5, 3);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `components`
+--
+ALTER TABLE `components`
+  ADD PRIMARY KEY (`component_id`);
+
+--
+-- Indexes for table `grade_templates`
+--
+ALTER TABLE `grade_templates`
+  ADD PRIMARY KEY (`template_id`),
+  ADD UNIQUE KEY `class_code` (`class_code`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `permissions`
@@ -153,6 +246,14 @@ ALTER TABLE `role_permissions`
   ADD KEY `role_id` (`role_id`);
 
 --
+-- Indexes for table `template_components`
+--
+ALTER TABLE `template_components`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `template_id` (`template_id`),
+  ADD KEY `component_id` (`component_id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -172,6 +273,18 @@ ALTER TABLE `user_roles`
 --
 
 --
+-- AUTO_INCREMENT for table `components`
+--
+ALTER TABLE `components`
+  MODIFY `component_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `grade_templates`
+--
+ALTER TABLE `grade_templates`
+  MODIFY `template_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `permissions`
 --
 ALTER TABLE `permissions`
@@ -184,14 +297,26 @@ ALTER TABLE `roles`
   MODIFY `role_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
+-- AUTO_INCREMENT for table `template_components`
+--
+ALTER TABLE `template_components`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `grade_templates`
+--
+ALTER TABLE `grade_templates`
+  ADD CONSTRAINT `grade_templates_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
 
 --
 -- Constraints for table `role_permissions`
@@ -199,6 +324,13 @@ ALTER TABLE `users`
 ALTER TABLE `role_permissions`
   ADD CONSTRAINT `role_permissions_ibfk_1` FOREIGN KEY (`permission_id`) REFERENCES `permissions` (`permission_id`),
   ADD CONSTRAINT `role_permissions_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `roles` (`role_id`);
+
+--
+-- Constraints for table `template_components`
+--
+ALTER TABLE `template_components`
+  ADD CONSTRAINT `template_components_ibfk_1` FOREIGN KEY (`template_id`) REFERENCES `grade_templates` (`template_id`),
+  ADD CONSTRAINT `template_components_ibfk_2` FOREIGN KEY (`component_id`) REFERENCES `components` (`component_id`);
 
 --
 -- Constraints for table `user_roles`
